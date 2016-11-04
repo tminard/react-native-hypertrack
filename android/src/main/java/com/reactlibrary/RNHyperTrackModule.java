f655ae270f95d1c3b672ac6d9de02cf8495237a5
Rishabh Garg <rishabh@hypertrack.io>
2016-11-05 04:19:33 +0530

    wrapper for Complete Task

--- android/src/main/java/com/reactlibrary/RNHyperTrackModule.java
+++ android/src/main/java/com/reactlibrary/RNHyperTrackModule.java
@@ -48,4 +48,38 @@ public class RNHyperTrackModule extends ReactContextBaseJavaModule {
   public void show(String message, int duration) {
     Toast.makeText(getReactApplicationContext(), message, duration).show();
   }
+
+  @ReactMethod
+  public void completeTask(String taskID, final Callback callback) {
+      Context context = getReactApplicationContext();
+      HTTransmitterService transmitterService = HTTransmitterService.getInstance(context);
+
+      transmitterService.completeTask(taskID, new HTCompleteTaskStatusCallback() {
+          @Override
+          public void onSuccess(String taskID) {
+              try {
+                  JSONObject result = new JSONObject();
+                  result.put("task_id", taskID);
+                  callback.invoke("Success");
+              } catch (JSONException e) {
+                  callback.invoke("");
+              }
+          }
+
+          @Override
+          public void onError(Exception e) {
+              try {
+                  JSONObject result = new JSONObject();
+                  if (e == null) {
+                      result.put("error", "");
+                  } else {
+                      result.put("error", e.toString());
+                  }
+                  callback.invoke(e.toString());
+              } catch (JSONException exception) {
+                  callback.invoke("");
+              }
+          }
+      });
+  }
 }
--- index.js
+++ index.js
@@ -3,4 +3,14 @@ import { NativeModules } from 'react-native';
 
 const { RNHyperTrack } = NativeModules;
 
-export default RNHyperTrack;
+module.exports = {
+	showToast(message: string, duration: double = 2000) {
+		RNHyperTrack.show(message, duration);
+	},
+	testMessage() {
+		return "React Native HyperTrack SDK!";
+	},
+	completeTask(taskId, callback) {
+		RNHyperTrack.completeTask(taskId, callback);
+	}
+}
