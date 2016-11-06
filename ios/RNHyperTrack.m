b074abb43b7a1df9abaca50452d87d56d92c1671
Arjun Attam <arjunattam@gmail.com>
2016-11-05 23:04:02 -0700

    iOS driver sdk methods

--- ios/RNHyperTrack.m
+++ ios/RNHyperTrack.m
@@ -12,25 +12,58 @@
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(startTrip:(NSString *)driverId
-                           :(NSArray *)taskIds
-                           :(RCTResponseSenderBlock)successCallback
-                           :(RCTResponseSenderBlock)failureCallback)
+                  :(NSArray *)taskIds
+                  :(RCTResponseSenderBlock)successCallback
+                  :(RCTResponseSenderBlock)failureCallback)
 {
-  HTTripParams* tripParams = [[HTTripParams alloc] init];
+    HTTripParams* tripParams = [[HTTripParams alloc] init];
+    tripParams.driverId = driverId;
+    tripParams.tasksIDs = taskIds;
+
+    [[HTTransmitterClient sharedClient] startTripWithTripParams:tripParams completion:^(HTTrip* trip, NSError* error) {
+        if (error) {
+            // Handle error and try again.
+            failureCallback();
+        } else {
+            // If there is no error, use the tripID received in the callback in your app.
+            successCallback();
+        }
+    }];
 }
 
 RCT_EXPORT_METHOD(completeTask:(NSString *)taskId
-                              :(RCTResponseSenderBlock)successCallback
-                              :(RCTResponseSenderBlock)failureCallback)
+                  :(RCTResponseSenderBlock)successCallback
+                  :(RCTResponseSenderBlock)failureCallback)
 {
-  // 
+    // Mark task as completed by passing taskID
+    [[HTTransmitterClient sharedClient] completeTaskWithTaskID:taskId completion:^(NSString* taskID, NSError* error) {
+
+        if (error) {
+            // Handle error and try again.
+            failureCallback();
+        } else {
+            // If there is no error, use the taskID received in the callback in your app.
+            successCallback();
+        }
+    }];
 }
 
 RCT_EXPORT_METHOD(endTrip:(NSString *)tripId
-                         :(RCTResponseSenderBlock)successCallback
-                         :(RCTResponseSenderBlock)failureCallback)
+                  :(RCTResponseSenderBlock)successCallback
+                  :(RCTResponseSenderBlock)failureCallback)
 {
-  //
+    // Warning: does not use the tripId
+
+    [[HTTransmitterClient sharedClient] endTripWithCompletion:^(HTTrip* trip, NSError* error) {
+
+        if (error) {
+            // Handle error and try again.
+            failureCallback();
+        } else {
+            // If there is no error, use the trip received in the callback in your app.
+            successCallback();
+        }
+    }];
 }
 
 @end
