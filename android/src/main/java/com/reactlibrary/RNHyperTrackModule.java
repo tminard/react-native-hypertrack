4e75bb94aca0686185651c3b8bf04f2fb5c17d4c
Rishabh Garg <rishabh@hypertrack.io>
2016-11-06 13:52:24 +0530

    Add startTrip/endTrip

--- android/src/main/java/com/reactlibrary/RNHyperTrackModule.java
+++ android/src/main/java/com/reactlibrary/RNHyperTrackModule.java
@@ -9,12 +9,27 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableArray;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
+
 
 import io.hypertrack.lib.common.HyperTrack;
 import io.hypertrack.lib.transmitter.service.HTTransmitterService;
+import io.hypertrack.lib.transmitter.model.HTTrip;
+import io.hypertrack.lib.transmitter.model.HTTripParams;
+import io.hypertrack.lib.transmitter.model.HTTripParamsBuilder;
+import io.hypertrack.lib.transmitter.model.callback.HTTripStatusCallback;
+import io.hypertrack.lib.transmitter.model.callback.HTCompleteTaskStatusCallback;
+
+import android.content.Context;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
 
 public class RNHyperTrackModule extends ReactContextBaseJavaModule {
 
@@ -27,7 +42,7 @@ public class RNHyperTrackModule extends ReactContextBaseJavaModule {
     super(reactContext);
     this.reactContext = reactContext;
 
-    HyperTrack.setPublishableApiKey("ACCOUNT_PK", getReactApplicationContext());
+    HyperTrack.setPublishableApiKey("pk_1507af78ef9dca2d250bdd6cf835e315bde4ad96", getReactApplicationContext());
     HTTransmitterService.initHTTransmitter(getReactApplicationContext());
   }
 
@@ -55,19 +70,31 @@ public class RNHyperTrackModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void completeTask(String taskID, final Callback callback) {
+  public void startTrip(String driverID, ReadableArray taskIDList, final Callback successCallback, final Callback failureCallback) {
       Context context = getReactApplicationContext();
       HTTransmitterService transmitterService = HTTransmitterService.getInstance(context);
 
-      transmitterService.completeTask(taskID, new HTCompleteTaskStatusCallback() {
+      ArrayList<String> taskIDs= this.toArrayList(taskIDList);
+
+      HTTripParams htTripParams = new HTTripParamsBuilder().setDriverID(driverID)
+              .setTaskIDs(taskIDs)
+              .setOrderedTasks(false)
+              .setIsAutoEnded(false)
+              .createHTTripParams();
+
+
+      transmitterService.startTrip(htTripParams, new HTTripStatusCallback() {
           @Override
-          public void onSuccess(String taskID) {
+          public void onSuccess(boolean isOffline, HTTrip htTrip) {
               try {
+                  Gson gson = new Gson();
+                  String tripJson = gson.toJson(htTrip);
                   JSONObject result = new JSONObject();
-                  result.put("task_id", taskID);
-                  callback.invoke("Success");
+                  result.put("is_offline", isOffline);
+                  result.put("trip", tripJson);
+                  successCallback.invoke("");
               } catch (JSONException e) {
-                  callback.invoke("");
+                  successCallback.invoke("");
               }
           }
 
@@ -80,11 +107,107 @@ public class RNHyperTrackModule extends ReactContextBaseJavaModule {
                   } else {
                       result.put("error", e.toString());
                   }
-                  callback.invoke(e.toString());
+                  failureCallback.invoke(e.toString());
               } catch (JSONException exception) {
-                  callback.invoke("");
+                  failureCallback.invoke("");
               }
           }
       });
+  }
+
+  @ReactMethod
+  public void endTrip(String tripID, final Callback successCallback, final Callback failureCallback) {
+       Context context = getReactApplicationContext();
+       HTTransmitterService transmitterService = HTTransmitterService.getInstance(context);
+
+       transmitterService.endTrip(tripID, new HTTripStatusCallback() {
+           @Override
+           public void onSuccess(boolean isOffline, HTTrip htTrip) {
+               try {
+                   Gson gson = new Gson();
+                   String tripJson = gson.toJson(htTrip);
+                   JSONObject result = new JSONObject();
+                   result.put("is_offline", isOffline);
+                   result.put("trip", tripJson);
+                   successCallback.invoke("");
+               } catch (JSONException e) {
+                   successCallback.invoke("");
+               }
+           }
+
+           @Override
+           public void onError(Exception e) {
+               try {
+                   JSONObject result = new JSONObject();
+                   if (e == null) {
+                       result.put("error", "");
+                   } else {
+                       result.put("error", e.toString());
+                   }
+                   failureCallback.invoke("");
+               } catch (JSONException exception) {
+                   failureCallback.invoke("");
+               }
+           }
+       });
+  }
+
+  @ReactMethod
+  public void completeTask(String taskID, final Callback successCallback, final Callback failureCallback) {
+      Context context = getReactApplicationContext();
+      HTTransmitterService transmitterService = HTTransmitterService.getInstance(context);
+
+      transmitterService.completeTask(taskID, new HTCompleteTaskStatusCallback() {
+          @Override
+          public void onSuccess(String taskID) {
+              try {
+                  JSONObject result = new JSONObject();
+                  result.put("task_id", taskID);
+                  successCallback.invoke("Success");
+              } catch (JSONException e) {
+                  successCallback.invoke("");
+              }
+          }
+
+          @Override
+          public void onError(Exception e) {
+              try {
+                  JSONObject result = new JSONObject();
+                  if (e == null) {
+                      result.put("error", "");
+                  } else {
+                      result.put("error", e.toString());
+                  }
+                  failureCallback.invoke(e.toString());
+              } catch (JSONException exception) {
+                  failureCallback.invoke("");
+              }
+          }
+      });
+  }
+
+  public ArrayList<String> toArrayList(ReadableArray taskIDs) {
+    ArrayList<String> arrayList = new ArrayList<>();
+    for (int i = 0; i < taskIDs.size(); i++) {
+      switch (taskIDs.getType(i)) {
+        case Null:
+          arrayList.add(null);
+          break;
+        case Boolean:
+          throw new IllegalArgumentException("Could not convert object at index: " + i + ".");
+        case Number:
+          throw new IllegalArgumentException("Could not convert object at index: " + i + ".");
+        case String:
+          arrayList.add(taskIDs.getString(i));
+          break;
+        case Map:
+          throw new IllegalArgumentException("Could not convert object at index: " + i + ".");
+        case Array:
+          throw new IllegalArgumentException("Could not convert object at index: " + i + ".");
+        default:
+          throw new IllegalArgumentException("Could not convert object at index: " + i + ".");
+      }
+    }
+    return arrayList;
   }
 }
